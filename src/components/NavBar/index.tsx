014817dbedf2475ca73665443309ed3793612cb4
Victoria <victoria.vasconcelos@hotmail.com>
2023-03-31 15:19:29 -0300

    feat: :art: add hamburger responsive in navbar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,10 +1,29 @@
+import { useState, useCallback } from 'react';
+
+import { useRouter } from 'next/router';
+import Image from 'next/image';
 import Lottie from 'react-lottie';
 
-import { Container, Logo, TitleLogo, Nav, NavRouters } from './styles';
+import { Container, Logo, TitleLogo, Nav, NavRouters, ModalHamburger, ContainerHamburger, NavHamburger } from './styles';
+
+import Hamburguer from "../../assets/img/svg/hamburguer.svg";
+import Close from "../../assets/img/svg/close.svg";
 
 import GameControllers from "../../assets/animations/game-controller.json";
 
 export default function NavBar ( ) {
+
+  const router = useRouter();
+  const [getHamburger, setHamburger] = useState(true);
+
+  const openHamburger = useCallback(() => {
+    setHamburger(true);
+  }, []);
+
+  const closeHamburger = useCallback(() => {
+    setHamburger(false);
+  }, []);
+
   const defaultOptionsGameControllersAnimation = {
     loop: false,
     autoplay: true, 
@@ -28,7 +47,19 @@ export default function NavBar ( ) {
         <NavRouters>Sobre</NavRouters>
         <NavRouters>Coach</NavRouters>
         <NavRouters>Contato</NavRouters>
+        <div onClick={() => setHamburger(!getHamburger)}>
+        {getHamburger ? (<Image src={Close} alt="" height="20" width="20"/>) : (<Image src={Hamburguer} alt="" height="20" width="20"/> ) }
+      </div>
       </Nav>
+      {getHamburger && (
+        <ModalHamburger>
+          <ContainerHamburger>
+            <NavHamburger>Sobre</NavHamburger>
+            <NavHamburger>Coach</NavHamburger>
+            <NavHamburger>Contato</NavHamburger>
+          </ContainerHamburger>
+        </ModalHamburger>
+      )}
     </Container>
   )
 }
--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div `
   display: flex;
+  position: relative;
   justify-content: space-between;
   padding: 1rem 5rem;
   align-items: center;
@@ -12,10 +13,10 @@ export const Container = styled.div `
     padding: 1rem 3rem;
   }
   @media (max-width: 650px) {
-    padding: 1rem 1.5rem;
+    padding: 1rem 1rem;
   }
   @media (max-width: 550px) {
-    /*criarhamburguer*/ 
+    padding: 1rem 0.7rem;
   }
 `
 ;
@@ -35,9 +36,48 @@ export const Nav = styled.div`
   display: flex;
   align-items: center;
   gap: 3rem;
+  div{
+    cursor: pointer;
+    display: none;
+    @media (max-width:550px) {
+      display: flex;
+    }
+  }
 `;
 
 export const NavRouters = styled.text`
+  color: #34495C;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+  @media (max-width: 550px) {
+    display: none;
+  }
+  :hover {
+    opacity: 0.6;
+  }
+`;
+
+export const ModalHamburger = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #66CDAA;
+  padding: 1rem; 
+  width: 8rem;
+  top: 3rem;
+  right: 0;
+`;
+
+export const ContainerHamburger = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const NavHamburger = styled.div`
   color: #34495C;
   font-size: 14px;
   font-weight: 600;
