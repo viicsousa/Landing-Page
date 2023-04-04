d44e040a78310849c7cc85bc70eb0c18bd01f300
Victoria <victoria.vasconcelos@hotmail.com>
2023-04-04 16:10:06 -0300

    fix: :zap: adjusted nextconfig for deploy vercel

--- src/pages/laddingPage/index.page.tsx
+++ src/pages/laddingPage/index.page.tsx
@@ -1,6 +1,6 @@
 import NavBar from '../../components/NavBar/index';
 import About from "../../components/About/index";
-export default function Home () {
+export default function LaddingPage () {
   return (
     <>
     <NavBar/>
