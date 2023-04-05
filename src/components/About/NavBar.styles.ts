c9255fc84d1270570174419d4f04337f6230512b
Victoria <victoria.vasconcelos@hotmail.com>
2023-04-05 15:26:15 -0300

    style: :zap: adjusted select

--- src/components/About/NavBar.styles.ts
+++ src/components/About/NavBar.styles.ts
@@ -2,8 +2,13 @@ import styled from "styled-components";
 
 export const Structure = styled.div`
   position: relative;
-  background: rgb(2,0,36);
-  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(52,73,80,1) 37%, rgba(14,173,207,1) 80%);
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    90deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(52, 73, 80, 1) 37%,
+    rgba(14, 173, 207, 1) 80%
+  );
   min-height: 110vh;
   #imgback {
     opacity: 0.5;
@@ -11,8 +16,7 @@ export const Structure = styled.div`
     position: absolute;
     width: 100%;
     min-height: 100%;
-  };
-
+  }
 `;
 export const Container = styled.div`
   display: flex;
@@ -23,8 +27,8 @@ export const Container = styled.div`
   @media (max-width: 500px) {
     padding: 2rem 1rem;
   }
-  `;
-  
+`;
+
 export const ContainerAbout = styled.div`
   display: flex;
   flex-direction: column;
@@ -36,11 +40,11 @@ export const ContainerAbout = styled.div`
 `;
 
 export const TitleContainer = styled.div`
-  font-weight:900;
+  font-weight: 900;
   -webkit-text-stroke-width: 1.7px;
-  -webkit-text-stroke-color: #34495C;
-  font-size: 3.5em; 
-  color: #66CDAA;
+  -webkit-text-stroke-color: #34495c;
+  font-size: 3.5em;
+  color: #66cdaa;
   @media (max-width: 500px) {
     font-size: 3rem;
   }
@@ -49,13 +53,13 @@ export const TitleContainer = styled.div`
 export const TextAbout = styled.div`
   text-align: justify;
   width: 30rem;
-  color: #34495C;
+  color: #34495c;
   font-size: 14px;
   font-weight: 600;
-  border: 7px double #34495C;
-  border-radius:8px;
+  border: 7px double #34495c;
+  border-radius: 8px;
   padding: 1rem;
-  background: #66CDAA;
+  background: #66cdaa;
   @media (max-width: 500px) {
     width: 18rem;
     font-size: 13px;
@@ -71,7 +75,6 @@ export const TypeCoach = styled.div`
     flex-direction: column;
     align-items: center;
   }
-  
 `;
 
 export const Coachs = styled.div`
@@ -81,16 +84,16 @@ export const Coachs = styled.div`
   align-items: center;
   justify-content: space-around;
   width: 15rem;
-  color: #34495C;
+  color: #34495c;
   font-size: 14px;
-  border: 7px double #34495C;
-  border-radius:8px;
+  border: 7px double #34495c;
+  border-radius: 8px;
   padding: 0.5rem;
-  background: #66CDAA;
+  background: #66cdaa;
   @media (max-width: 500px) {
     width: 12rem;
     font-size: 12px;
-  };
+  } ;
 `;
 
 export const TitleCoach = styled.div`
@@ -107,14 +110,13 @@ export const Value = styled.div`
   text-align: center;
 `;
 
-
 export const Form = styled.div`
-  background: #FFFFFF;
+  background: #ffffff;
   display: flex;
   flex-direction: column;
   width: 20rem;
   padding: 1rem;
-  border-radius: 8px; 
+  border-radius: 8px;
   margin-top: 3rem;
 `;
 export const ContainerForm = styled.div`
@@ -127,63 +129,60 @@ export const Title = styled.form`
   text-align: center;
   font-size: 24px;
   font-weight: 600;
-  color: #34495C;
+  color: #34495c;
 `;
-export const Information =styled.div`
+export const Information = styled.div`
   display: flex;
   flex-direction: column;
 `;
 export const Label = styled.label`
-  color: #34495C;
+  color: #34495c;
 `;
 export const Input = styled.input`
-  background: #FFFFFF;
+  background: #ffffff;
   .hover {
     color: red;
-    }
-  ::focus{
+  }
+  ::focus {
     outline: none;
-    }
-    ::placeholder {
-      font-size: 14px;
-      color: #CDCDCD;
-    }
+  }
+  ::placeholder {
+    font-size: 14px;
+    color: #cdcdcd;
+  }
 `;
 
 export const Select = styled.select`
-font-size: 14px;
-background: #FFFFFF;
-color: #34495C;
+  font-size: 14px;
+  background: #ffffff;
+  color: #34495c;
   .hover {
     color: red;
   }
-  :focus{
+  :focus {
     outline: none;
   }
-
-  
 `;
 
 export const CoachI = styled.option`
   font-size: 14px;
-  background: #FFFFFF;
-  color: #34495C;
+  color: #34495c;
 `;
 
 export const CoachII = styled.option`
   font-size: 14px;
-  background: #FFFFFF;
+  color: #34495c;
 `;
 
 export const CoachIII = styled.option`
   font-size: 14px;
-  background: #FFFFFF;
+  color: #34495c;
 `;
 
 export const Button = styled.button`
-  background-color: #66CDAA;
+  background-color: #66cdaa;
   border: none;
-  color: #34495C;
+  color: #34495c;
   font-weight: 600;
   border-radius: 8px;
   padding: 0.2rem;
@@ -196,7 +195,7 @@ export const Alert = styled.div`
   top: 25px;
   right: 15px;
   padding: 1rem;
-  background: #FFFFFF;
+  background: #ffffff;
   color: red;
   animation-duration: 0.2s;
   animation-name: slidein;
@@ -211,6 +210,5 @@ export const Alert = styled.div`
     to {
       width: 10rem;
     }
-    
   }
 `;
